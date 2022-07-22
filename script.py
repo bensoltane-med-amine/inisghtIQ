8ef77536e5f69540f071fc23908c75cd9133a1e2
bensoltane-med-amine <bensoltane.mohammed.amine@gmail.com>
2022-07-22 20:55:47 +0100

    set env vars

--- script.py
+++ script.py
@@ -1,12 +1,14 @@
-from boto3.session import Session
 import boto3
 import os
+from decouple import config
 
-ACCESS_KEY = 'AKIA3KWFUA6S7R3H2ZPQ'
-SECRET_KEY = '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'
+ACCESS_KEY = config('ACCESS_KEY_ID')
+SECRET_KEY = config('SECRET_ACCESS_KEY')
 
+def download_from_s3():
+    session = boto3.client('s3', aws_access_key_id=ACCESS_KEY , aws_secret_access_key=SECRET_KEY)
+    session.download_file('iiqtest','1636994821620_insightiq_export_1636727116.zip',os.path.join(os.getcwd(), "1636994821620_insightiq_export_1636727116.zip"))
 
-session = boto3.client('s3', aws_access_key_id=ACCESS_KEY , aws_secret_access_key=SECRET_KEY)
+def upload_to_s3():
+    print("ok")
 
-
-session.download_file('iiqtest','1636994821620_insightiq_export_1636727116.zip',os.path.join(os.getcwd(), "1636994821620_insightiq_export_1636727116.zip"))
