eb55d5fed6ab62a0a0df101e6a799d3b7908860c
bensoltane-med-amine <bensoltane.mohammed.amine@gmail.com>
2022-07-22 21:00:20 +0100

    upload json config

--- script.py
+++ script.py
@@ -6,9 +6,16 @@ ACCESS_KEY = config('ACCESS_KEY_ID')
 SECRET_KEY = config('SECRET_ACCESS_KEY')
 
 def download_from_s3():
-    session = boto3.client('s3', aws_access_key_id=ACCESS_KEY , aws_secret_access_key=SECRET_KEY)
-    session.download_file('iiqtest','1636994821620_insightiq_export_1636727116.zip',os.path.join(os.getcwd(), "1636994821620_insightiq_export_1636727116.zip"))
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY , aws_secret_access_key=SECRET_KEY)
+    s3.download_file('iiqtest','1636994821620_insightiq_export_1636727116.zip',os.path.join(os.getcwd(), "1636994821620_insightiq_export_1636727116.zip"))
 
 def upload_to_s3():
-    print("ok")
+    s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY , aws_secret_access_key=SECRET_KEY)
+    s3.upload_file(
+    Filename="insightiq_export_1636727116/DASHISILON_6805ca0e04067358a752ac03776323443e24_config.json",
+    Bucket="iiqtest",
+    Key="DASHISILON_6805ca0e04067358a752ac03776323443e24_config.json",
+)
+
+upload_to_s3()
 
